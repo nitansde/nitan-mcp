bf741543c2eeb98588fa29915ce4770f8ab3fcdc
Nitan SDE <tinge-23update@icloud.com>
2025-10-25 20:56:02 -0500

    add tool discourse_list_top_topics

--- src/tools/registry.ts
+++ src/tools/registry.ts
@@ -16,6 +16,7 @@ import { registerCreateUser } from "./builtin/create_user.js";
 import { registerListUserPosts } from "./builtin/list_user_posts.js";
 import { registerListHotTopics } from "./builtin/list_hot_topics.js";
 import { registerListNotifications } from "./builtin/list_notifications.js";
+import { registerListTopTopics } from "./builtin/list_top_topics.js";
 
 export type ToolsMode = "auto" | "discourse_api_only" | "tool_exec_api";
 
@@ -49,6 +50,7 @@ export async function registerAllTools(
   registerListUserPosts(server, ctx, { allowWrites: false });
   registerListHotTopics(server, ctx, { allowWrites: false });
   registerListNotifications(server, ctx, { allowWrites: false });
+  registerListTopTopics(server, ctx, { allowWrites: false });
   registerFilterTopics(server, ctx, { allowWrites: false });
   registerCreatePost(server, ctx, { allowWrites: opts.allowWrites });
   registerCreateUser(server, ctx, { allowWrites: opts.allowWrites });
